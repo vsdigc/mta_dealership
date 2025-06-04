22cc2d3197226abfd0f6e7e87acb9c1c7abf24a7
DiogoCidra <diogocidra811@gmail.com>
2025-06-04 23:07:23 +0100

    Create importante.lua

---
+++ importante.lua
@@ -0,0 +1,36 @@
+--[[
+
+FEITO POR DOGGOZ!!!!!!!!
+
+]]--
+
+-- Para alterar os veículos, preços e id's dos veículos da concessionária, alterar nesta tabela no arquivo main-g.lua
+
+    vehicles = {
+
+        {name = 'Pop 100', price = 500, id = 462};
+        {name = 'Fiat Punto', price = 1000, id = 565};
+        {name = 'Peugeot 208', price = 3000, id = 602};
+        {name = 'BMW E36', price = 30000, id = 445};
+        {name = 'Audi A3', price = 70000, id = 402};
+        {name = 'BMW M8', price = 200000, id = 411};
+        {name = 'Mercedes AMG GT', price = 500000, id = 451};
+        {name = 'Lamborghini Huracan', price = 1500000, id = 415};
+        {name = 'Ferrari Italia', price = 2000000, id = 477};
+        {name = 'Bugatti Chiron', price = 3000000, id = 506};
+
+    },
+
+-- Para alterar a posição do blip da loja ou garagem. Caso queiras adicionar mais, apenas adicionam uma nova linha na tabela com novas coordenadas.
+
+    shops = {
+
+        {2131.84473, -1150.17993, 24.18600}
+
+    },
+
+    garages = {
+
+        {2148.91968, -1162.12659, 23.82031}
+
+    },
